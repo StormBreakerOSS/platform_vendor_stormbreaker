365d9fd02280c5ca724c18832fe2eee74302d3d7
Arseniy Graur <argraur@aosip.dev>
2020-02-12 09:52:38 +0000

    stormbreaker: Add fonts for customization and set Google Sans as default font.
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>
    Signed-off-by: danascape <saalim.priv@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -18,6 +18,9 @@
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/stormbreaker/overlay
 
+# Inherit from fonts config
+$(call inherit-product, vendor/stormbreaker/config/fonts.mk)
+
 # Inherit from our versioning
 $(call inherit-product, vendor/stormbreaker/config/versioning.mk)
 
