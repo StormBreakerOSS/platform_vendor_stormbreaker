cca703df1771abd11092da156f5b9f646ad96908
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: Add custom soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -25,3 +25,33 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += stormbreakerGlobalVars
+SOONG_CONFIG_stormbreakerGlobalVars += \
+    additional_gralloc_10_usage_bits \
+    target_init_vendor_lib
+
+SOONG_CONFIG_NAMESPACES += stormbreakerQcomVars
+SOONG_CONFIG_stormbreakerQcomVars += \
+    supports_extended_compress_format \
+    uses_qti_camera_device
+
+# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_stormbreakerQcomVars += \
+    qcom_soong_namespace
+endif
+
+# Soong bool variables
+SOONG_CONFIG_stormbreakerQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_stormbreakerQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+
+# Soong value variables
+SOONG_CONFIG_stormbreakerGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_stormbreakerGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_stormbreakerQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif
