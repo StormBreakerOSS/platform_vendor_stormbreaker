b608edb8e1ef9d947130aa67aa88320b0c1be937
Alessandro Astone <ales.astone@gmail.com>
2021-01-05 18:04:14 +0100

    vendor: Commonize display_intf_headers for QSSI supported platforms
    Change-Id: I7224c10295c4123222d81b92f7829502e3cc2d61

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -36,10 +36,10 @@ SOONG_CONFIG_stormbreakerQcomVars += \
     supports_extended_compress_format \
     uses_qti_camera_device
 
-# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+# Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_stormbreakerQcomVars += \
-    qcom_soong_namespace
+    qcom_display_headers_namespace
 endif
 
 # Soong bool variables
@@ -52,6 +52,9 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 # Soong value variables
 SOONG_CONFIG_stormbreakerGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_stormbreakerGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
-ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_stormbreakerQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+
+ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_stormbreakerQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
+else
+SOONG_CONFIG_stormbreakerQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
